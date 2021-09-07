b2f2ab0c5ae55a692e0fb945f72cf9a35bee05c1
Mohamed <mohamed.h.abukar@gmail.com>
2021-09-07 16:44:12 -0500

    updated file.

--- src/components/profileImage/index.js
+++ src/components/profileImage/index.js
@@ -4,7 +4,7 @@ import profileImg from "./IMG_2978.JPG";
 export default function ProfileImage () {
     return (
         <div className="profilepic">
-            <img src={profileImg} alt ="Profile picture" height={300} width={300} />
+            <img src={profileImg} alt ="Profile picture" height={600} width={400} />
         </div>
     )
 }
