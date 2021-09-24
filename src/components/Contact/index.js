526e7f34e88915019f4597a64745b8da10411268
Mohamed <mohamed.h.abukar@gmail.com>
2021-09-23 22:04:04 -0500

    updated files.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,83 @@
-import React from "react";
+import React, { useState } from 'react';
+
+import { validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
-    return (
-        <>
-            <div>
-                <h2>Contact Me</h2>
-                <ul>
-                    <li><strong>Email:</strong> mohamed.h.abukar@gmail.com"</li>
-                    <li><strong>Github:</strong><a href="https://github.com/mohamedabukar"  target="_blank" rel="noreferrer">My Github</a></li>
-                    <li><strong>My phone number: </strong> 6126440078</li>
-                    <li><strong>Linkdin: </strong><a href="https://www.linkedin.com/in/mohamed-h-abukar/"
-                        target="_blank" rel="noreferrer">My Linkdin</a></li>
-                </ul>
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
 
-            </div>
-        </>
-    )
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
+  return (
+    <section>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
+
