b4ad7771551eaa8f0267b5d68834d01ad253c870
Mohamed <mohamed.h.abukar@gmail.com>
2021-09-28 18:05:48 -0500

    footer removed from the page.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css"
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import Body from './components/body';
 
-function App() {
+export default function App() {
   return (
-    <>
       <Body />
-      <Footer />
-    </>
     );
 }
 
-export default App;
