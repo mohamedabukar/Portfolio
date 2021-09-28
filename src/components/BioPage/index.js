2f8b23fad8e4d153d57687f83adbcfd95349538f
Mohamed <mohamed.h.abukar@gmail.com>
2021-09-28 17:52:02 -0500

    Page structure changed.

--- src/components/BioPage/index.js
+++ src/components/BioPage/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./style.css";
-import profileImage from "../ProfileImage";
 import ProfileImage from "../ProfileImage";
 
 
 export default function BioPage() {
     return (
         <>
-            <div>
-                <ProfileImage />
-                <h2>About Me</h2>
-                <p>My name is Mohamed Abukar and I am a 20 year old Full Stack Web Developer from Apple Valley, MN.</p>
-                <h3>Education</h3>
-                <p>I enrolled in the Full Stack Web Developement bootcamp that the Unviversity of Minnesota was offering. In this bootcamp we learned important skills such as creating applications containing jquery, javascript, react, css and html. It was a 12 week intensive course which was 20 hours a week of class time.</p>
+            <div className="bio-container row">
+                <div className="bio-section col-4">
+                    <h2>About Me</h2>
+                    <p>My name is Mohamed Abukar and I am a 20 year old Full Stack Web Developer from Apple Valley, MN.</p>
+                </div>
+                <div className="bio-section col-4">
+                    <ProfileImage />
+                </div>
+                <div className="bio-section col-4">
+                    <h3>Education</h3>
+                    <p>I enrolled in the Full Stack Web Developement bootcamp that the Unviversity of Minnesota was offering. In this bootcamp we learned important skills such as creating applications containing jquery, javascript, react, css and html. It was a 12 week intensive course which was 20 hours a week of class time.</p>
+                </div>
             </div>
         </>
     )
