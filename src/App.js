2fd7bfc3188ae3b2ee77107230925ae6f69988b1
Mohamed <mohamed.h.abukar@gmail.com>
2021-09-29 14:22:50 -0500

    footer added.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css"
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 import Body from './components/body';
 
 export default function App() {
   return (
+    <div>
       <Body />
+      <Footer />
+    </div>
+      
     );
 }
 
