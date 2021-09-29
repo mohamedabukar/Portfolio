6f8b261fb03d1665534eb8be674cfdcd201758be
Mohamed <mohamed.h.abukar@gmail.com>
2021-09-29 14:23:55 -0500

    footer links created.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,13 @@
-// import React from "react";
+import React from "react";
+import "./style.css";
 
-// export default function Footer() {
-//     return (
-//         <footer className="footer">
-//             <div className="footer-container text-center">
-//                <p>&#169; Portfolio</p>
-//             </div>
-//         </footer>
-//     );
-// }
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-container text-center">
+               <a href="https://github.com/mohamedabukar">Github </a>
+               <a href="https://www.linkedin.com/in/mohamed-h-abukar/"> linkedin</a>
+            </div>
+        </footer>
+    );
+}
