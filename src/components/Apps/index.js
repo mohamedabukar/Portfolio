52c1c8148586083cffd4fe7ec6adb0acc99e8407
Mohamed <mohamed.h.abukar@gmail.com>
2021-09-29 16:11:47 -0500

    apps structure changed.

--- src/components/Apps/index.js
+++ src/components/Apps/index.js
@@ -1,39 +1,42 @@
-import React, { useState } from "react";
-import Project from "../Project";
+import React from "react";
+import actor from "../../images/actor-database.jpg";
+import track from "../../images/one-track.jpg";
+import smiley from "../../images/smiley-job-recruiter.jpg";
 import "./main.css";
 
 export default function Apps() {
 
-    const [projects] = useState([
-        {
-            name: 'Actor Database',
-            description: 'Database for information on Actors movies',
-            link: "https://mohamedabukar.github.io/actor-database/",
-            repo: "https://github.com/mohamedabukar/actor-database"
-        },
-        {
-            name: 'One Track Music Database',
-            description: 'Application used for personalizing music playlists',
-            link: "https://arcane-castle-03954.herokuapp.com/",
-            repo: "https://github.com/mohamedabukar/one-track"
-        },
-        {
-            name: 'Smiley Job Recruiter',
-            description: 'Website for users looking for a job',
-            link: "https://safe-journey-56448.herokuapp.com/",
-            repo: "https://github.com/mohamedabukar/group-pj3-smileyrecruiter"
-        }
-    ]);
     return (
-        <div>
-            <div className="flex-row">
-                {projects.map((project, idx) => (
-                    <Project
-                        project={project}
-                        key={"project" + idx}
-                    />
-                ))}
-            </div>
-        </div>
+        <>
+            <section className="apps">
+                <div className="project">
+                    <a href="https://m-ray-ofsunshine.github.io/actor-database/" target="_blank" rel="noreferrer">
+                        <img src={actor} width="320" height="270" alt="actor database" />
+                    </a>
+                    <h3>Actor Database</h3>
+                    <a href="https://github.com/m-ray-ofSunshine/actor-database" target="_blank" rel="noreferrer">
+                        <p>Github Repository</p>
+                    </a>
+                </div>
+                <div className="project">
+                    <a href="https://arcane-castle-03954.herokuapp.com/" target="_blank" rel="noreferrer">
+                        <img src={track} width="320" height="270" alt="one track" />
+                    </a>
+                    <h3>One Track Music</h3>
+                    <a href="https://github.com/tedheikkila/one-track" target="_blank" rel="noreferrer">
+                        <p>Github Repository</p>
+                    </a>
+                </div>
+                <div className="project">
+                    <a href="https://safe-journey-56448.herokuapp.com/" target="_blank" rel="noreferrer">
+                        <img src={smiley} width="320" height="270" alt="smiley" />
+                    </a>
+                    <h3>Smiley Recruiter</h3>
+                    <a href="https://github.com/JChosay/group-pj3-smileyrecruiter.git" target="_blank" rel="noreferrer">
+                        <p>Github Repository</p>
+                    </a>
+                </div>
+            </section>
+        </>
     )
 }
