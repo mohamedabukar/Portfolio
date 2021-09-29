9c88a9fc80ed328ff829b69901d3def83c0a2c55
Mohamed <mohamed.h.abukar@gmail.com>
2021-09-28 22:09:24 -0500

    project buttons created.

--- src/components/Apps/index.js
+++ src/components/Apps/index.js
@@ -1,43 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Project from "../Project";
 import "./main.css";
 
 export default function Apps() {
+
+    const [projects] = useState([
+        {
+            name: 'Actor Database',
+            description: 'Database for information on Actors movies',
+            link: "https://mohamedabukar.github.io/actor-database/",
+            repo: "https://github.com/mohamedabukar/actor-database"
+        },
+        {
+            name: 'One Track Music Database',
+            description: 'Application used for personalizing music playlists',
+            link: "https://arcane-castle-03954.herokuapp.com/",
+            repo: "https://github.com/mohamedabukar/one-track"
+        },
+        {
+            name: 'Smiley Job Recruiter',
+            description: 'Website for users looking for a job',
+            link: "https://safe-journey-56448.herokuapp.com/",
+            repo: "https://github.com/mohamedabukar/group-pj3-smileyrecruiter"
+        }
+    ]);
     return (
-        <>
-            <section className="apps">
-                <div className="project">
-                    <a href="https://m-ray-ofsunshine.github.io/actor-database/" target="_blank" rel="noreferrer">
-                        <div className="project1"></div>
-                    </a>
-                    <h2>Actor Database</h2>
-                    <p>This is an actor database that allows you to search an actors and get information about them such as their best movies, bio, and twitter feed.</p>
-                    <p>Click image for deployed application</p>
-                    <a href="https://github.com/m-ray-ofSunshine/actor-database" target="_blank" rel="noreferrer">
-                        <p>Github Repository</p>
-                    </a>
-                </div>
-                <div className="project">
-                    <a href="https://arcane-castle-03954.herokuapp.com/" target="_blank" rel="noreferrer">
-                        <div className="project2"></div>
-                    </a>
-                    <h2>One Track Music Database</h2>
-                    <p>This is a music application that allows you to search up songs and create playlists by saving songs into your own profile.</p>
-                    <p>Click on image for deployed application</p>
-                    <a href="https://github.com/tedheikkila/one-track" target="_blank" rel="noreferrer">
-                        <p>Github Repository</p>
-                    </a>
-                </div>
-                <div className="project">
-                    <a href="https://safe-journey-56448.herokuapp.com/" target="_blank" rel="noreferrer">
-                        <div className="project3"></div>
-                    </a>
-                    <h2>Smiley Recruiter</h2>
-                    <p>THis is an application that allows a user to search for available jobs at their position and create profile to save jobs that they are interested in.</p>
-                    <a href="https://github.com/JChosay/group-pj3-smileyrecruiter.git" target="_blank" rel="noreferrer">
-                        <p>Github Repository</p>
-                    </a>
-                </div>
-            </section>
-        </>
+        <div>
+            <div className="flex-row">
+                {projects.map((project, idx) => (
+                    <Project
+                        project={project}
+                        key={"project" + idx}
+                    />
+                ))}
+            </div>
+        </div>
     )
 }
