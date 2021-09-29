ed4457396e61349b55624043ef114c1a99832b40
Mohamed <mohamed.h.abukar@gmail.com>
2021-09-29 14:23:19 -0500

    contact form changed.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,81 +3,110 @@ import React, { useState } from 'react';
 import { validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
-  const [formState, setFormState] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
-
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
+  const [successMessage, setSuccessMessage] = useState('')
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log('Submit Form', formState);
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'name') {
+      setName(inputValue);
+    }
+    else if (inputType === 'email') {
+      setEmail(inputValue);
     }
   };
 
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
-    }
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+      if (!name) {
+    setErrorMessage('Please provide your name')
+      return
+    } else if (!validateEmail(email)) {
+      setErrorMessage('Please enter a valid email');
+      return;
+    } 
+
+    setSuccessMessage('Thanks. Contact me via email:')
+
+    setErrorMessage('')
+
+    setName('');
+    setEmail('');
+
   };
 
   return (
-    <section>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            defaultValue={name}
-            onBlur={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input
-            type="email"
-            name="email"
-            defaultValue={email}
-            onBlur={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            name="message"
-            rows="5"
-            defaultValue={message}
-            onBlur={handleChange}
-          />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
+    <div className="form-container">
+      <h3 id="form-greeting">Hi {name}</h3>
+      <br></br>
+      <p>all fields required</p>
+      <form className="form">
+        <div className="form-group row">
+          <label htmlFor="input-name" className="col-sm-2 col-form-label">Name</label>
+          <div className="col-sm-8">
+            <input
+              value={name}
+              name="name"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Full Name"
+              id="input-name"
+              className="form-control"
+            />
           </div>
-        )}
-        <button type="submit">Submit</button>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="input-email" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-8">
+            <input
+              className="form-control"
+              id="input-email"
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              type="email"
+              placeholder="email@mail.com"
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="input-message" className="col-sm-2 col-form-label">Message</label>
+          <div className="col-sm-8">
+            <input
+              className="form-control"
+              id="input-message"
+              name="message"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="message"
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <div className="col-sm-10 offset-sm-2">
+            <button type="button" className="btn btn-primary" id="form-btn" onClick={handleFormSubmit}>Submit</button>
+          </div>
+        </div>
       </form>
-    </section>
+
+      {successMessage && (
+        <div>
+          <h5 className="success-text">{successMessage}</h5>
+          <h5 className="email"><a href="mailto:mohamed.h.abukar@gmail.com">mohamed.h.abukar@gmail.com</a></h5>
+        </div>
+      )}
+
+      {errorMessage && (
+        <div>
+          <h5 className="error-text">{errorMessage}</h5>
+        </div>
+      )}
+    </div>
   );
 }
-
